868c749e6bb9b67523843e7764c4527143c25098
sifatanan <sanan6576@bths.edu>
2022-09-21 16:11:23 -0400

    Added fav food and movie

--- src/Sample.java
+++ src/Sample.java
@@ -1,5 +1,8 @@
 public class Sample {
     public static void main(String[] args) {
+
         System.out.println("Sifat");
+        System.out.println("Spider Man 2");
+        System.out.println("Chicken over Rice");
     }
 }
